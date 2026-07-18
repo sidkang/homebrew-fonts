3a61dd3550ea7e57de7cfd17fb13e61b84486124
sid@cube <sidskang@gmail.com>
2026-07-18 21:43:40 +0800

    fix(release): preserve artifacts for cask updates

--- Casks/font-braille-spinner-kitty-term.rb
+++ Casks/font-braille-spinner-kitty-term.rb
@@ -1,8 +1,8 @@
 cask "font-braille-spinner-kitty-term" do
-  version "1.0.0"
-  sha256 "8a6a754552917c834009e62bab862f304439c312273391b658d8a586257ce80a"
+  version "1.0.2"
+  sha256 "cca902bd1a658ede087b12f136dd3623af4e90d82913e4bbb7504be66036813e"
 
-  url "https://github.com/sidkang/fonts/releases/download/braille-spinner-kitty-term-v#{version}/BrailleSpinnerKittyTerm-#{version}.zip"
+  url "https://github.com/sidkang/homebrew-fonts/releases/download/braille-spinner-kitty-term-v#{version}/BrailleSpinnerKittyTerm-#{version}.zip"
   name "Braille Spinner Kitty Term"
   desc "Fixed-grid private-use Braille spinner glyphs for Kitty terminals"
   homepage "https://github.com/sidkang/homebrew-fonts/tree/master/fonts/braille-spinner-kitty-term"
